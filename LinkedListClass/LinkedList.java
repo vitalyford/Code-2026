474574759e866a406cdcbbd390d642f24242b2b0
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2026-09-15 10:22:01 -0400

    Add default get method

--- LinkedListClass/LinkedList.java
+++ LinkedListClass/LinkedList.java
@@ -24,9 +24,20 @@ public class LinkedList {
         head = null;
     }
 
-    // public ... get(int index) {
-        
-    // }
+    /**
+     * The method takes an index as an int and returns the respective value.
+     * Returns null if the index is out of bounds.
+     * @param index the passed index of the item you want to retrieve
+     * @return the actual value by that index
+     */
+    public Object get(int index) { // index == 2
+        Node curr = head;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+
+        return curr.value;
+    }
 
     /**
      * This method adds to the BEGINNING of the LinkedList
