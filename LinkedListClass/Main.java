3090689f8b918aad249d9ec0835a52a6e5e892d7
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2026-09-15 09:52:28 -0400

    Add a Main class that runs a test for LinkedList addStart method

---
+++ LinkedListClass/Main.java
@@ -0,0 +1,15 @@
+package LinkedListClass;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        
+
+        ll.addStart(7);
+        ll.addStart(-1);
+        ll.addStart(9);
+        ll.addStart(3);
+        ll.addStart(10);
+        ll.addStart(8);
+    }
+}
